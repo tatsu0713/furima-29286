08f8eb2427486e2511f37ef59362d1dd0ec63d43
tatsu0713 <kwbttk@icloud.com>
2020-09-01 23:53:06 +0900

    バリデーションとストロングパラメーターの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
   has_many :items
   has_many :item_purchases
 
+  validates :nickname, presence: true
+  validates :kanji_family_name, presence: true, format: { with: /\A[ぁ-んァ-ンー-龠]+\z/}
+  validates :kanji_first_name, presence: true, format: { with: /\A[ぁ-んァ-ンー-龠]+\z/}
+  validates :kana_family_name, presence: true, format: { with: /\A[ァ-ヶー−]+\z/}
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー−]+\z/}
+  validates :birthday, presence: true
 end
