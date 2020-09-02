7bf25d4828d23f338090244ceb846f42a64e3e8b
tatsu0713 <kwbttk@icloud.com>
2020-09-03 01:14:35 +0900

    画面の遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
 
   private
 
@@ -11,6 +9,7 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :kanji_family_name, :kanji_first_name, :kana_family_name, :kana_first_name, :brthday])
   end
