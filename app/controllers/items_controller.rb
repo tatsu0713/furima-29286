2a0309782a2690a8c869ed620f82f92380198333
tatsu0713 <kwbttk@icloud.com>
2020-09-09 20:31:27 +0900

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
 
   def index
-    @item = Item.all.order("created_at DESC")
+    @item = Item.all.order('created_at DESC')
   end
 
   def new
@@ -18,7 +18,6 @@ class ItemsController < ApplicationController
     end
   end
 
-
   private
 
   def item_params
@@ -26,8 +25,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to "/users/sign_in"
-    end
+    redirect_to '/users/sign_in' unless user_signed_in?
   end
 end
