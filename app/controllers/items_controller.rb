c3a2b6de019f8842df7bbd13331b6438aa18601f
tatsu0713 <kwbttk@icloud.com>
2020-09-11 12:54:39 +0900

    before_actionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @item = Item.all.order('created_at DESC')
@@ -10,15 +11,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path
@@ -44,5 +42,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to '/users/sign_in' unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
