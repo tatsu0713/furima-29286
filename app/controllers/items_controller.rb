4cc318ba20164773fd29b93dcf05a5d274e6faa5
tatsu0713 <kwbttk@icloud.com>
2020-09-11 14:18:10 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,7 +18,7 @@ class ItemsController < ApplicationController
 
   def update
     @item.update(item_params)
-    if @item.save
+    if @item.valid?
       redirect_to item_path
     else
       render :edit
